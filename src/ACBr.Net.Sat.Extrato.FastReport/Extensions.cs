bac83156b6ebd6314bd29a8aaf887101f6968530
Rafael Teno Dias <rfteno@gmail.com>
2021-08-09 14:11:08 -0400

    [*] Atualização de dependências. [+] Adicionado o projeto de impressão em Fast para este repositorio.

---
+++ src/ACBr.Net.Sat.Extrato.FastReport/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using ACBr.Net.Core.Extensions;
+
+namespace ACBr.Net.Sat.Extrato.FastReport
+{
+
+    public static class Extensions
+    {
+        public static string Descricao(this CodigoMP codigo)
+        {
+            switch (codigo)
+            {
+                case CodigoMP.Dinheiro: return "Dinheiro";
+                case CodigoMP.Cheque: return "Cheque";
+                case CodigoMP.CartaodeCredito: return "Cartão de Crédito";
+                case CodigoMP.CartaodeDebito: return "Cartão de Débito";
+                case CodigoMP.CreditoLoja: return "Crédito Loja";
+                case CodigoMP.ValeAlimentacao: return "Vale Alimentação";
+                case CodigoMP.ValeRefeicao: return "Vale Refeição";
+                case CodigoMP.ValePresente: return "Vale Presente";
+                case CodigoMP.ValeCombustivel: return "Vale Combustível";
+                case CodigoMP.Outros: return "Outros";
+                default: return "Outros";
+            }
+        }
+
+    }
+
+}
---
+++ src/ACBr.Net.Sat.Extrato.FastReport/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ACBr.Net S@T Extrato FastReport")]
+[assembly: AssemblyDescription("ACBr.Net S@T Extrato FastReport")]
+[assembly: AssemblyProduct("ACBr.Net S@T Extrato FastReport")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ACBr.Net")]
+[assembly: AssemblyCopyright("Copyright © ACBr.Net 2014 - 2016")]
+[assembly: AssemblyTrademark("Projeto ACBr.Net https://github.com/ACBrNet")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7660a32a-e629-4755-9639-b2d323b5543d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
